0aeafaf5150dcca29203d02a9c173e43a725c088
Jobayer_ak <jobayer.ak@gmail.com>
2023-06-22 13:20:40 +0600

    working on searchbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
           />
 
           <p className="text-base text-gray-700">
-            Carhut 2023 <br />
+            CarHub 2023 <br />
             All rights reserved &copy;
           </p>
         </div>
@@ -39,7 +39,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-        <p>@2023 CarHut. All Rights Reserved</p>
+        <p>@2023 CarHub. All Rights Reserved</p>
         <div className="footer_copyrights-link">
           <Link href="/" className="text-gray-500">
             Privacy Policy
